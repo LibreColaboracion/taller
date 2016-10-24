5292a720500f9a7906aff515ee3301885f5fe19e
Moi Rouhs <carsrouhs@gmail.com>
2016-10-24 18:50:42 +0000

    comentarios en python

--- python/ejercicios.py
+++ python/ejercicios.py
@@ -1,2 +1,10 @@
 #! /usr/bin/python3
+
+# En python un comentario de una lena se hace con el simbolo numeral '#'
+
+''' 
+    El comentario de multiples lineas se hace mediante comilla triple tanto al principio como al final
+    Los comentarios son muy importantes ya que nos permite explicar el código que se elabora y así continuar con el trabajo déspues de varios meses, por tal motivo deben ser claros, consisos y explicativos con respecto al código que involucra.
+'''
+
 print ('hola')
