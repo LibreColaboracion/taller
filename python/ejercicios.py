9854243b9531f8d2eb4d7022a0b6dd2156f19dff
Moi Rouhs <carsrouhs@gmail.com>
2016-10-24 23:36:06 +0000

    Mensaje en la terminal con rb, py y js

--- python/ejercicios.py
+++ python/ejercicios.py
@@ -6,5 +6,6 @@
     El comentario de multiples lineas se hace mediante comilla triple tanto al principio como al final
     Los comentarios son muy importantes ya que nos permite explicar el código que se elabora y así continuar con el trabajo déspues de varios meses, por tal motivo deben ser claros, consisos y explicativos con respecto al código que involucra.
 '''
+# 1. Mensaje en la terminal
 
 print ('hola')
