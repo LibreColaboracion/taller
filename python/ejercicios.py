76bfdca1332312a36496a94200aad8b0b3516bf5
Moi Rouhs <carsrouhs@gmail.com>
2016-10-25 00:57:49 +0000

    Pequeña explicación de variables

--- python/ejercicios.py
+++ python/ejercicios.py
@@ -3,9 +3,29 @@
 # En python un comentario de una lena se hace con el simbolo numeral '#'
 
 ''' 
-    El comentario de multiples lineas se hace mediante comilla triple tanto al principio como al final
+    El comentario de multiples lineas se hace mediante comilla triple tanto al principio como al final.
     Los comentarios son muy importantes ya que nos permite explicar el código que se elabora y así continuar con el trabajo déspues de varios meses, por tal motivo deben ser claros, consisos y explicativos con respecto al código que involucra.
 '''
-# 1. Mensaje en la terminal
 
-print ('hola')
+# 1. Mensaje en la terminal
+print ('hola') # print solo da salto de linea al final de la imprimir lainformación requerida.
+
+# 2. Variables
+'''
+    Las variables son contenedores de información y pueden contener:
+        1. Numerica que puede ser entero como 2 o de coma flotante 'float' 3.2
+        2. Cadenas de texto 'string' que deben ir siempre dentro de comillas simples o dobles.
+        3. Boleanas es decir de verdadero 'true' o falso 'false'.
+    En python no hace falta ninguna palabra especial o convención para designar una variable solamente se escribe su contenido déspues de un igual. 
+'''
+
+# 2.1 Variables numericas
+numerica = 10.52
+
+# 2.2 Variables string
+texto = 'Cadena de texto'
+
+# 2.3 Variables boleanas
+boleana = True
+
+print( numerica, texto, boleana)
