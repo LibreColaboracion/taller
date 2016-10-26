6f55464fa16805e54d83b3aa380445f7506ee299
Moi Rouhs <carsrouhs@gmail.com>
2016-10-26 01:53:06 +0000

    Explicación de variables y métodos 1 para python

--- python/ejercicios.py
+++ python/ejercicios.py
@@ -10,7 +10,7 @@
 # 1. Mensaje en la terminal
 print ('hola') # print solo da salto de linea al final de la imprimir lainformación requerida.
 
-# 2. Variables
+# 2. Variables 1
 '''
     Las variables son contenedores de información y pueden contener:
         1. Numerica que puede ser entero como 2 o de coma flotante 'float' 3.2
@@ -28,4 +28,20 @@ texto = 'Cadena de texto'
 # 2.3 Variables boleanas
 boleana = True
 
-print( numerica, texto, boleana)
+print( numerica, texto, boleana) # Las variables no necesitan comillas para poder ser mostradas en la terminal.
+
+# Métodos
+'''
+    Es un bloque de código que se invoca mediante una palabra clave y afecta o modifica en este caso la variable indicada, los metodos pueden ser creados por uno mismo pero en este caso se usaran los que viene por defecto en el sistema, especialmente los metodos de manejo de texto.
+'''
+
+# Método sin argumentos
+palabra = 'programador'
+
+print(palabra.upper()) # En este caso el metodo es upper(), el cual recahe sobre la variable palabra que contiene el string programador
+
+# Método con argumentos
+print(palabra.split('o')) # El metodo split('ARG') corta la cadena de texto donde encuentre el ARGumento dado, en este caso la vocal 'o' y por esta vocal divide la palabra.
+
+# Método donde el argumento es la varibale a modificar
+print(len(palabra)) # El metodo len() toma como argumento la misma variable o cadena de texto de la cual queremos saber su largo.
