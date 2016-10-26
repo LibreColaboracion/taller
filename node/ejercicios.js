b5a67b1d44cbe9897f757e4bb03082bd2c80d03c
Moi Rouhs <carsrouhs@gmail.com>
2016-10-26 00:40:20 +0000

    Explicacion variables y metodos 1 para js

--- node/ejercicios.js
+++ node/ejercicios.js
@@ -9,7 +9,7 @@
 // 1. Mensaje en la terminal
 console.log("hola") // console.log solo da salto de linea al final de la imprimir lainformación requerida.
 
-// 2. Variables
+// 2. Variables 1
 /*
     Las variables son contenedores de información y pueden contener:
         1. Numerica que puede ser entero como 2 o de coma flotante 'float' 3.2
@@ -27,4 +27,17 @@ var texto = 'Cadena de texto';
 // 2.3 Variables boleanas
 var boleana = true;
 
-console.log(numerica, texto, boleana)
+console.log(numerica, texto, boleana) // Las variables no necesitan comillas para poder ser mostradas en la terminal.
+
+// Métodos
+/*
+    Es un bloque de código que se invoca mediante una palabra clave y afecta o modifica en este caso la variable indicada, los metodos pueden ser creados por uno mismo pero en este caso se usaran los que viene por defecto en el sistema, especialmente los metodos de manejo de texto.
+*/
+
+// Método para saber el largo de una palabra
+var palabra = "programador";
+
+console.log(palabra.length) // En este caso el metodo es length, la cual recahe sobre la variable palabra que contiene el string programador
+
+//Método con argumentos
+console.log(palabra.split('o')); // el metodo split('ARG') corta la cadena de texto donde encuentre el ARGumento dado, en este caso la vocal 'o' y por esta vocal divide la palabra
